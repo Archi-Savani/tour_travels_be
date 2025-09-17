9be82f5b1ee8cc556de0ab847ba3e9af72a1fb5f
Archi-Savani <aaru.jbs07@gmail.com>
2025-09-17 14:37:06 +0530

    inquiry add

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 const userRoutes = require("./routers/User");
 const stateRoutes = require("./routers/State");
 const tourRoutes = require("./routers/Tour");
+const inquiryRoutes = require("./routers/Inquiry");
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", userRoutes);
 app.use("/api/state", stateRoutes);
 app.use("/api/tour", tourRoutes);
+app.use("/api/inquiry", inquiryRoutes);
 
 const connectDB = async () => {
     try {
