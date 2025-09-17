10c39359434d2e3d4340ff807ad55f218c4a9d6f
Archi-Savani <aaru.jbs07@gmail.com>
2025-09-17 14:37:18 +0530

    inquiry add

---
+++ routers/Inquiry.js
@@ -0,0 +1,32 @@
+// routes/inquiryRoutes.js
+const express = require("express");
+const router = express.Router();
+const {
+    createInquiry,
+    getInquiries,
+    getInquiryById,
+    updateInquiry,
+    deleteInquiry,
+} = require("../controllers/Inquiry");
+
+// Import your auth middleware
+const auth  = require("../middleware/auth");
+
+// Routes
+
+// Create a new inquiry (protected)
+router.post("/", auth, createInquiry);
+
+// Get all inquiries (protected)
+router.get("/", getInquiries);
+
+// Get single inquiry by ID (protected)
+router.get("/:id", getInquiryById);
+
+// Update an inquiry by ID (protected)
+router.put("/:id",auth, updateInquiry);
+
+// Delete an inquiry by ID (protected)
+router.delete("/:id", deleteInquiry);
+
+module.exports = router;
