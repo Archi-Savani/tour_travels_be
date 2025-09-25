0348540c089e5d709bf223e5cfe2cb981fbf1932
HARSHAD <harshadkakadiya0096@gmail.com>
2025-09-25 16:56:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Debug middleware to log requests
 app.use((req, res, next) => {
